e95bcd980f5e0e03bc588504a53ae4ab1ec7b8c1
sfmqur <sammaruska@gmail.com>
2024-03-02 21:35:57 -0600

    line endings fix

---
+++ LorcanaLogic.Contracts/Interfaces/IDeck.cs
@@ -0,0 +1,11 @@
+﻿namespace LorcanaLogic.Contracts
+{
+    public interface IDeck
+    {
+        string Name { get; }
+        List<ICard> CardList { get; }
+        List<int> CardQuantity { get; }
+
+        string Diff(IDeck other);
+    }
+}
---
+++ LorcanaLogic/Deck.cs
@@ -0,0 +1,30 @@
+﻿using LorcanaLogic.Contracts;
+
+namespace LorcanaLogic
+{
+    public class Deck : IDeck
+    {
+        public string Name { get; }
+
+        public List<ICard> CardList { get; }
+
+        public List<int> CardQuantity { get; }
+
+        public Deck(string deckListPath)
+        {
+            Name = deckListPath.Substring(deckListPath.LastIndexOf("\\"), deckListPath.LastIndexOf('.'));
+            CardList = new List<ICard>();
+            CardQuantity = new List<int>();
+            importDeckFromDreambornExport(deckListPath);
+        }
+        public string Diff(IDeck other)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void importDeckFromDreambornExport(string deckListPath)
+        {
+
+        }
+    }
+}
