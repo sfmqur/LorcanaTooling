c92a17adcfe7b35ca6c3ad19a3b3cce48ea0bf1a
sfmqur <sammaruska@gmail.com>
2024-03-11 18:21:18 -0500

    override comparision operators on card.

--- LorcanaLogic/Card.cs
+++ LorcanaLogic/Card.cs
@@ -1,10 +1,11 @@
-﻿using LorcanaLogic.Contracts;
+﻿using System.Diagnostics;
+using LorcanaLogic.Contracts;
 
 namespace LorcanaLogic
 {
     public class Card : ICard
     {
-        public string Name { get; } 
+        public string Name { get; }
 
         public int Set { get; }
 
@@ -14,20 +15,21 @@ namespace LorcanaLogic
 
         public Rarity Rarity { get; }
 
-        public decimal Price { get  ; set  ; }
-        public decimal PriceFoil { get  ; set  ; }
-        public bool Inkable { get  ; set  ; }
-        public int Cost { get  ; set  ; }
-        public int Strength { get  ; set  ; }
-        public int Willpower { get  ; set  ; }
-        public int Lore { get  ; set  ; }
-        public string Text { get  ; set  ; }
-        public CardType Type { get  ; set  ; }
-        public string TypeText { get  ; set  ; }
+        public decimal Price { get; set; }
+        public decimal PriceFoil { get; set; }
+        public bool Inkable { get; set; }
+        public int Cost { get; set; }
+        public int Strength { get; set; }
+        public int Willpower { get; set; }
+        public int Lore { get; set; }
+        public string Text { get; set; }
+        public CardType Type { get; set; }
+        public string TypeText { get; set; }
         public int QuantNormal { get; set; }
         public int QuantFoil { get; set; }
 
-        public Card(string name, int set, int cardNo, Color color, Rarity rarity, int quantNorm, int quantFoil, decimal price=0, decimal foilPrice=0) {
+        public Card(string name, int set, int cardNo, Color color, Rarity rarity, int quantNorm, int quantFoil, decimal price = 0, decimal foilPrice = 0)
+        {
             Name = name;
             Set = set;
             CardNo = cardNo;
@@ -39,6 +41,48 @@ namespace LorcanaLogic
             QuantFoil = quantFoil;
             Text = "";
             TypeText = "";
+        }
+
+        public override string ToString()
+        {
+            return Name; 
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+            return (obj as Card)?.Set == Set && (obj as Card)?.CardNo == CardNo;
+        }
+        public static bool operator <(Card card1, Card card2)
+        {
+            if (card1.Equals(card2)) return false;
+            if (card1.Set < card2.Set) return true;
+            if (card1.Set > card2.Set) return false;
+            if (card1.CardNo < card2.CardNo) return true; //knoow that set is equal here. 
+            return false;
+        }
+        public static bool operator >(Card card1, Card card2)
+        {
+            if (card1.Equals(card2)) return false;
+            if (card1.Set > card2.Set) return true;
+            if (card1.Set < card2.Set) return false;
+            if (card1.CardNo > card2.CardNo) return true; //knoow that set is equal here. 
+            return false; 
+        }
+        
+
+        public static bool operator <=(Card card1, Card card2)
+        {
+            if (card1.Equals(card2)) return true;
+            return card1 < card2;
+        }
+        public static bool operator >=(Card card1, Card card2)
+        {
+            if (card1.Equals(card2)) return true;
+            return card1 > card2;
         }
     }
 }
