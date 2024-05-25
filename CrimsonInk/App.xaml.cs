08f0e05e6418a62e32f5eea0240be1277f7971df
sfmqur <sammaruska@gmail.com>
2024-05-25 08:59:53 -0500

    crimsonINkwpf project created

---
+++ CrimsonInk/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace CrimsonInk
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
